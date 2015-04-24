6ae04597039a7686fc75e0aea10aa9e92a8f5a99
Noah Emrich <noah.emrich@gmail.com>
2015-04-24 17:16:50 -0400

    debugging 500 server error

--- php/demo.php
+++ php/demo.php
@@ -5,13 +5,13 @@ define('DB_USER', 'b1e4408afef9b2');
 define('DB_PASSWORD', '2651c64d');
 define('DB_HOST', 'us-cdbr-iron-east-02.cleardb.net:3306');
 
-$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD);
 
-if (!$link) {
-	die('Could not connect: ' . mysql_error());
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 
-$db_selected = mysql_select_db(DB_NAME, $link);
+$db_selected = mysql_select_db(DB_NAME, $mysqli);
 
 if (!$db_selected) {
 	die('Can\'t use ' . DB_NAME . ': ' . mysql_error());
