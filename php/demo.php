22a95963f952dc58c82cac6af8a297e88e77ecb7
Noah Emrich <noah.emrich@gmail.com>
2015-04-24 15:59:48 -0400

    added local jquery library

--- php/demo.php
+++ php/demo.php
@@ -5,14 +5,13 @@ define('DB_USER', 'b1e4408afef9b2');
 define('DB_PASSWORD', '2651c64d');
 define('DB_HOST', 'us-cdbr-iron-east-02.cleardb.net:3306');
 
-$link = new mysqli(DB_HOST, DB_USER, DB_PASSWORD);
+$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
 
-if (!$link->connect_errno) {
-	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+if (!$link) {
+	die('Could not connect: ' . mysql_error());
 }
 
-
-$db_selected = $mysqli->select_db(DB_NAME, $link);
+$db_selected = mysql_select_db(DB_NAME, $link);
 
 if (!$db_selected) {
 	die('Can\'t use ' . DB_NAME . ': ' . mysql_error());
@@ -23,9 +22,9 @@ $value2 = $_POST['text'];
 
 $sql = "INSERT INTO demo (email, text) VALUES ('$value', '$value2')";
 
-if (!$mysqli->query($sql)) {
-	die('Error: ' . $mysqli->connect_errno());
+if (!mysql_query($sql)) {
+	die('Error: ' . mysql_error());
 }
 
-mysqli_close($sql_link);
+mysql_close();
 ?>
