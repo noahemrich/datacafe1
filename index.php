7d5ca82e951abe55e474bc07c8c9d31e7033703f
Noah Emrich <noah.emrich@gmail.com>
2015-04-24 13:11:04 -0400

    added local jquery library

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <link rel="stylesheet" type="text/css" href="css/normalize.css" />
   <link rel="stylesheet" type="text/css" href="css/demo.css" />
   <link rel="stylesheet" type="text/css" href="css/component.css" />
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="just/jquery.min.js"></script>
   <script src="js/modernizr.custom.js"></script>
   <script src="js/countable.js"></script>
   <script src="js/classie.js"></script>
